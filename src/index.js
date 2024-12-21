d9aaf82592b0f8a79d8e4534dba9f5cfac0650b1
Lovienia Kamusasa <klovienia@gmail.com>
2024-12-21 02:37:29 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ function generatePoem(event){
     event.preventDefault();
 
     new Typewriter("#poem", {
-      strings: "        Oh, the autumn breeze, it whispers low",
+      strings: "Oh, the autumn breeze, it whispers low",
       autoStart: true,
       delay: 1,
       cursor: "",
@@ -10,4 +10,4 @@ function generatePoem(event){
 }
 
 let poemElement = document.querySelector("#poem-generator-form");
-poemElement.addEventListener("click", generatePoem);
+poemElement.addEventListener("submit", generatePoem);
