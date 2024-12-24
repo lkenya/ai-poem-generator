90abed5640e4e62f70a3b92188bebba27fe5406d
Lovienia Kamusasa <klovienia@gmail.com>
2024-12-25 00:45:55 +0200

    Improved the User experience

--- src/index.js
+++ src/index.js
@@ -17,6 +17,10 @@ function generatePoem(event) {
     "You are a romantic expert and love to write short poems.Your mission is to generate a 4 line poem with basic HTML and separate each line with a <br />. Do not use a title. Make sure to follow the user instructions below:.Sign the poem with 'SheCodes AI' inside a <strong></strong> element at the end of the poem";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating  poem about ${instructionsInput.value}</div>`;
+
   console.log("generating poem");
   console.log(`Prompt: ${prompt}`);
   console.log(`Context: ${context}`);
